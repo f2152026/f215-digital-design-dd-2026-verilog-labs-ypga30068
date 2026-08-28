38386b78d5a711148ace9c8cde5f43dd46f99ecc
YPGA30068 <f20250068@pilani.bits-pilani.ac.in>
2026-08-28 13:30:06 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -18,8 +18,9 @@ module FA_Gate(
 
   xor (ps,  a,   b);
   and (pc1, a,   b);
-  xor (sum, cin, ps);
-  and (pc2, cin, ps);
   or  (cout, pc1, pc2);
+  and (pc2, cin, ps);
+  xor (sum, cin, ps);
+
 
 endmodule
