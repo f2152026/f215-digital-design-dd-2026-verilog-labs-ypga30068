2a91f136692085e8090043e2984fa438873bd42f
YPGA30068 <f20250068@pilani.bits-pilani.ac.in>
2026-08-28 13:57:21 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -26,10 +26,10 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 //single const pair delay of 2(rise) and 5(fall)
-  xor #(2,5) (ps,  a,   b);
-  and #(2,5) (pc1, a,   b);
-  xor #(2,5) (sum, cin, ps);
-  and #(2,5) (pc2, cin, ps);
-  or #(2,5) (cout, pc1, pc2);
+  xor #(2) (ps,  a,   b);
+  and #(2) (pc1, a,   b);
+  xor #(2) (sum, cin, ps);
+  and #(2) (pc2, cin, ps);
+  or #(2) (cout, pc1, pc2);
 
 endmodule
