f443dede4edf3c1df9affce069183db1609d2d47
YPGA30068 <f20250068@pilani.bits-pilani.ac.in>
2026-08-29 06:29:58 +0000

    lab01 task4

--- labs/lab01/task4/dut.v
+++ labs/lab01/task4/dut.v
@@ -17,9 +17,9 @@ module dut(
   // rca64 U_IMPL (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
 
   // ---- Option 2: flat (unblocked) 64-bit carry-lookahead adder ----
-   cla64_flat U_IMPL (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
+  // cla64_flat U_IMPL (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
 
   // ---- Option 3: blocked 64-bit carry-lookahead adder ----
-  // cla64_blocked U_IMPL (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
+   cla64_blocked U_IMPL (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
 
 endmodule
